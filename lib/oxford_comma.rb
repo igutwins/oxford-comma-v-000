1f8926b4ab7eaf30d4da4e3ab8668652d461735f
Ian Gutwinski <iangutwinski@gmail.com>
2019-02-02 15:21:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,4 @@
 def oxford_comma(array)
-  if array.length = 1
-    array
-  elsif array.length = 2
-    array[0]+" and "+array[1]
-  else
-  array[-1] = "and "+array[-1]
-  array.join(", ")
-end
-end
-
-def oxford_comma(array)
   last_term = array.pop
   array.join(",")+" and "+last_term
 end
